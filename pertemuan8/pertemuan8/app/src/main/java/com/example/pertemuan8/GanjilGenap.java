3724979c988d8cd597ac69ffc8fb4ea5615755ef
Nadiroh <nadiroh25@gmail.com>
2019-07-11 16:23:00 +0800

    pertemuan8

---
+++ pertemuan8/pertemuan8/app/src/main/java/com/example/pertemuan8/GanjilGenap.java
@@ -0,0 +1,13 @@
+package com.example.pertemuan8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GanjilGenap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ganjil_genap);
+    }
+}
