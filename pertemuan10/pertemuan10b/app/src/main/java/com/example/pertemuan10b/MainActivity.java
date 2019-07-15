7004316ab13766b0e77d9ce87cc2647402654767
Nadiroh <nadiroh25@gmail.com>
2019-07-15 22:46:17 +0800

    pertemuan 10

--- pertemuan10/pertemuan10b/app/src/main/java/com/example/pertemuan10b/MainActivity.java
+++ pertemuan10/pertemuan10b/app/src/main/java/com/example/pertemuan10b/MainActivity.java
@@ -1,13 +1,145 @@
 package com.example.pertemuan10b;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUESE_CODE_STORAGE = 100;
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Aplikasi Catatan Proyek 1");
+        listview = findViewById(R.id.listView);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, InsertAndViewActivity.class);
+                Map<String, Object> data = (Map<String, Object>)parent.getAdapter().getItem(position);
+                intent.putExtra("filename ",data.get("nama").toString());
+                Toast.makeText(MainActivity.this,"You clicked"+ data.get("nama"),Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
+
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Map<String, Object> data = (Map<String, Object>)parent.getAdapter().getItem(position);
+                tampilkanDialogKonfirmasiHapusCatatan(data.get("nama").toString());
+                return true;
+            }
+        });
     }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (periksaIzinPenyimpanan()) {
+                mengambilListFilePadaFolder();
+            }
+        }else {
+            mengambilListFilePadaFolder();
+        }
+    }
+
+    public boolean periksaIzinPenyimpanan(){
+        if (Build.VERSION.SDK_INT >= 23){
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
+                return true;
+            }else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUESE_CODE_STORAGE);
+                return false;
+            }
+        }else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        switch (requestCode){
+            case REQUESE_CODE_STORAGE:
+                if (grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    mengambilListFilePadaFolder();
+            }
+            break;
+        }
+    }
+
+    void mengambilListFilePadaFolder(){
+        String path = Environment.getExternalStorageDirectory().toString()+" Kominfo_proyek";
+        File directory = new File(path);
+
+        if (directory.exists()){
+            File[] files = directory.listFiles();
+            String[] filenames = new  String[files.length];
+            String[] dateCreated = new String[files.length];
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MM YYYY HH:mm:ss");
+            ArrayList<Map<String,Object>> itemDataList = new ArrayList<Map<String,Object>>();
+
+            for (int i=0;i<files.length;i++){
+                filenames[i] = files[i].getName();
+                Date lastModDate = new Date(files[i].lastModified());
+                dateCreated[i] = simpleDateFormat.format(lastModDate);
+                Map<String, Object> listItemMap = new HashMap<>();
+                listItemMap.put("name", filenames[i]);
+                listItemMap.put("date", dateCreated[i]);
+                itemDataList.add(listItemMap);
+            }
+
+            SimpleAdapter simpleAdapter = new SimpleAdapter(this,itemDataList,android.R.layout.simple_list_item_2,new String[]{"nama","date"},new int[]{android.R.id.text1,android.R.id.text2});
+            listview.setAdapter(simpleAdapter);
+            simpleAdapter.notifyDataSetChanged();
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_tambah:
+                Intent intent = new Intent(this,InsertAndViewActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return  super.onOptionsItemSelected(item);
+    }
+
+    void tampilkanDialogKonfirmasiHapusCatatan(final String filename){
+        new AlertDialog.Builder(this)
+                .setTitle("Hapus Catatan ini?")
+                .setMessage("Apakah Anda yakin ingin menghapus Catatan"+filename)
+    }
+
+
 }
